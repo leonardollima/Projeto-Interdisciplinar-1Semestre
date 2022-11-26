72ba228a298b5491c3e32d79f7a1cf24c05d1cab
leonardollima <leonardo.lima68@fatec.sp.gov.br>
2022-11-25 21:18:17 -0300

    Ajustes no CSS de Contraste

---
+++ js/contraste.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let url1 = "../css/estilo.css"
+    let url2 = "../css/contraste.css"
+    let contraste = document.getElementById("contraste")
+    let semcontraste = document.getElementById("semcontraste")
+
+    function createTag(url){
+        let estilo = document.createElement("link")
+        estilo.href = url
+        estilo.rel = "stylesheet"
+        estilo.type = "text/css"
+        estilo.className = "acess"
+        
+        document.body.appendChild(estilo)
+    }
+
+    let tag = document.getElementsByClassName("acess") //<link  href=url rel="stylesheet" type="text/css">
+
+    var q = 0
+    
+    if(q != 0){
+         
+        q++
+        contraste.onclick = function(){
+            tag[0].href = url2
+            localStorage.setItem("css", url2)
+        }
+    }else{
+        contraste.onclick = function(){
+            tag[0].href = url1
+            localStorage.setItem("css", url1)
+        }
+    }
+    
+
+    let css = localStorage.getItem("css")
+
+    if(!css){
+        createTag(url1)
+    }else{
+        createTag(css)
+    }
+
+
+})
